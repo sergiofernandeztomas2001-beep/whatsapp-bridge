fd68bf2d320a9235cbe34261ced980845dd78d34
sergiofernandeztomas2001-beep <sergiofernandeztomas2001@gmail.com>
2026-04-24 09:09:41 +0200

    Add webhook handler for GET and POST requests

---
+++ api/webhook.js
@@ -0,0 +1,21 @@
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    const verify_token = 'grupospg_token_2026';
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode === 'subscribe' && token === verify_token) {
+      return res.status(200).send(challenge);
+    }
+
+    return res.status(403).send('Forbidden');
+  }
+
+  if (req.method === 'POST') {
+    console.log('EVENT RECEIVED:', JSON.stringify(req.body, null, 2));
+    return res.status(200).send('OK');
+  }
+
+  return res.status(405).send('Method Not Allowed');
+}
